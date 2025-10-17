a547ee14f9a2df3ad75088355815b1a92a9fe9ba
razel-dm <razielmaymons448@gmail.com>
2025-10-17 14:09:49 +0300

    new  file and big prosses on server

--- server.py
+++ server.py
@@ -14,5 +14,25 @@ class JsonSocket:
         self.sock = None
         self.connect()
     def connect(self):
+        while True:
+            try:
+                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.connect((self.host, self.port))
+                print(f"Connected to server at {self.host}:{self.port}")
+                break
+            except ConnectionRefusedError:
+                print(f"no connection, retrying in {RETRY_DELAY} ")
+                time.sleep(RETRY_DELAY)
 
-    
+    def send_json(self, data):
+        message = json.dumps(data).encode('utf-8')
+        self.sock.sendall(message)
+
+    def receive_json(self):
+        received_data = self.sock.recv(4096)
+        return json.loads(received_data.decode('utf-8'))
+
+    def close(self):
+        if self.sock:
+            self.sock.close()
+            print("Connection closed.")
