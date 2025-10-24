28c03b0d43021dc0234ec084cf9703445b95e24d
razel-dm <razielmaymons448@gmail.com>
2025-10-24 11:57:30 +0300

    make_the_actual_class

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,4 +1,25 @@
+import io
+import subprocess
+import sys
+import pyautogui
+
+
+def ensure_pyautogui():
+    try:
+        import pyautogui
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "pyautogui"])
+        import pyautogui
+    return pyautogui
+
+def take_screenshot(filename):
+    pyautogui = ensure_pyautogui()
+    screenshot = pyautogui.screenshot()
+    screenshot.save(filename)
 class ScreenshotTaker:
     def take_screenshot(self):
-        # Placeholder for screenshot taking logic
-        return b'screenshot_data'
+    
+        screenshot = pyautogui.screenshot()
+        screenshot_data = io.BytesIO()
+        screenshot.save(screenshot_data, format='PNG')
+        return screenshot_data.getvalue()
