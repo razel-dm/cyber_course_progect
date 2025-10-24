c93d8a23f0c923a67d380da5da807eec5fc1b52c
razel-dm <razielmaymons448@gmail.com>
2025-10-24 11:48:33 +0300

    edvace_and_change_name

---
+++ get_rick_roll.py
@@ -0,0 +1,9 @@
+import os
+import webbrowser
+class RickRoll:
+    def play_in_terminal(self):
+        os.system('CURL ASCII.LIVE/CAN-YOU-HEAR-ME')
+
+    def get_rick_roll(self):
+        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        webbrowser.open(url)
